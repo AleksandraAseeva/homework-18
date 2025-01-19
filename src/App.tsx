b581d7b2643f3ad4d752981e77b930ae59ef1769
Aleksandra Aseeva <aseevaaleksandra1@gmail.com>
2025-01-19 16:54:18 +0300

    Deploy3

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,16 @@ import { router } from "./route";
 import "./App.css";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor } from "./Redux/store";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from 'react-router-dom';
 
 function App() {
   return (
     <Provider store={store}>
-       <PersistGate persistor={persistor}>
+      <PersistGate persistor={persistor}>
         текст
-        <BrowserRouter basename="/homework-18">
-            <RouterProvider router={router} />
-          </BrowserRouter>
+        <HashRouter basename="/homework-18">
+          <RouterProvider router={router} />
+        </HashRouter>
       </PersistGate>
     </Provider>
   );
