23c664fb87d86e253638c6e2f678d1c619bde8cb
AleksandraAseeva <aleksandrplatc5@gmail.com>
2025-01-19 18:22:30 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import "./App.css";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor } from "./Redux/store";
 // import { HashRouter } from 'react-router-dom';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
@@ -14,9 +14,9 @@ function App() {
       <PersistGate persistor={persistor}>
         текст
         {/* <HashRouter basename="/homework-18"> */}
-        <BrowserRouter basename="/homework-18/">
+{/*         <BrowserRouter basename="/homework-18/"> */}
           <RouterProvider router={router} />
-          </BrowserRouter>
+{/*           </BrowserRouter> */}
         {/* </HashRouter> */}
       </PersistGate>
     </Provider>
